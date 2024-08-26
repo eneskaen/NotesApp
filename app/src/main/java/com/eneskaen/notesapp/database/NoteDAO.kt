4e87d6693097ef1efbda60498a2c7fd87226b12d
eneskaen <eneskaen94@gmail.com>
2024-08-26 16:29:36 +0300

    Project created. Dependencies, xml layouts, MVVM architecture Entity Class and Data Access Object created.

---
+++ app/src/main/java/com/eneskaen/notesapp/database/NoteDAO.kt
@@ -0,0 +1,31 @@
+package com.eneskaen.notesapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.eneskaen.notesapp.model.Note
+
+@Dao
+interface NoteDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Query("select * from notes order by id desc")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("select * from notes where noteTitle like :query or noteBody like :query")
+    fun search(query: String?) : LiveData<List<Note>>
+
+
+}
---
+++ app/src/main/java/com/eneskaen/notesapp/model/Note.kt
@@ -0,0 +1,18 @@
+package com.eneskaen.notesapp.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int,
+
+    val noteTitle : String,
+
+    val NoteBody : String
+):Parcelable
